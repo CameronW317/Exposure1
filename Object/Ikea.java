ce67899774e6e43fa359f2125417bb523606d12c
15029 <cameron.weiss@vtsd.com>
2025-06-03 13:11:08 -0400

    good

--- Object/Ikea.java
+++ Object/Ikea.java
@@ -1,4 +1,14 @@
 package Object;
 
 public class Ikea {
+    public static void main(String[] args) {
+        // dataType name = value
+
+        Table table1 = new Table();
+
+
+
+        System.out.println(table1.getWeight());
+    }
+
 }
--- Object/Table.java
+++ Object/Table.java
@@ -7,12 +7,11 @@ package Object;
 
 public class Table {
     // Attributes - Variables
-    double height;
-    double width;
-    double length;
-    double weight;
-    double price;
-
+    private double height;
+    private double width;
+    private double weight;
+    private double price;
+    private double length;
 
     // Default Constructor - Special Method
     public Table(){
@@ -23,6 +22,14 @@ public class Table {
         length = 0;
     }
 
+    public Table(double height, double width, double weight, double price, double length){
+        this.height = height;
+        this.width = width;
+        this.weight = weight;
+        this.price = price;
+        this.length = length;
+    }
+
     // Getters/Accessors
     public double getHeight() {
         return height;
@@ -30,13 +37,35 @@ public class Table {
     public double getWidth() {
         return width;
     }
-    public double getLength() {
-        return length;
-    }
     public double getWeight() {
         return weight;
     }
     public double getPrice() {
         return price;
+    }
+    public double getLength() {
+        return length;
+    }
+
+    // Setters/Mutators
+    public void setHeight(double height){
+        if(weight <= 1000) {
+            this.weight = height;
+        }
+        else{
+            this.weight = 1000;
+        }
+    }
+    public void setWidth(double width){
+        this.width = width;
+    }
+    public void setWeight(double weight){
+        this.weight = weight;
+    }
+    public void setPrice(double price){
+        this.price = price;
+    }
+    public void setLength(double length){
+        this.length = length;
     }
 }
